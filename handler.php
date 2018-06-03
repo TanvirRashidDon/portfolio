062f6c9edbb87364f0855d715f90f72dabfda425
don <tanvirrashiddon@gmail.com>
2018-06-04 03:38:15 +0600

    media quary added

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('tanvirrashiddon@gmail.com','tanvirrashid_don@yahoo.com'); // ← Your email here
+$pp->sendEmailTo(['tanvirrashiddon@gmail.com','tanvirrashid_don@yahoo.com']); // ← Your email here
 
 echo $pp->process($_POST);
